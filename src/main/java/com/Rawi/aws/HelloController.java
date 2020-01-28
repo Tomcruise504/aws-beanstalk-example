d38c116fbc5c8113ce94054fc8a28f2f71ff7949
Tomcruise504 <jackthepirate94@gmail.com>
2020-01-28 19:31:59 +0530

    AWS Beanstalk Initial commit

---
+++ src/main/java/com/Rawi/aws/HelloController.java
@@ -0,0 +1,12 @@
+package com.Rawi.aws;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/ hello")
+@RestController
+public class HelloController {
+    public String hello(){
+        return "Hello B@lu";
+    }
+}
---
+++ src/test/java/com/Rawi/aws/AwsElasticBeanstalkExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Rawi.aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsElasticBeanstalkExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
